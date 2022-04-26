68b5f4f6bf26de4422d86bff0cb06f413fbb0d74
pei <peipeilong116@gmail.com>
2022-04-25 23:57:15 -0400

    refactor: update assets screen

--- src/screens/Assets/Assets.tsx
+++ src/screens/Assets/Assets.tsx
@@ -5,7 +5,7 @@ import {
   tokensSelector,
 } from '@app/store/coins/coinsSelector';
 import {normalizeNumber} from '@app/utils';
-import React from 'react';
+import React, {useState} from 'react';
 import {Image, RefreshControl, TouchableOpacity, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 import {t} from 'react-native-tailwindcss';
@@ -13,11 +13,23 @@ import {useDispatch, useSelector} from 'react-redux';
 import MIcon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {colors} from '@app/assets/colors.config';
 import {refreshWallets} from '@app/store/wallets/actions';
+import {BaseCoin} from '@app/models';
+
+const shadow = {
+  shadowColor: colors.secondary,
+  shadowOffset: {
+    width: 0,
+    height: 0,
+  },
+  shadowOpacity: 1,
+  shadowRadius: 6,
+
+  elevation: 6,
+};
 
 export const AssetsScreen = () => {
   const isLoading = useSelector(isLoadingTokensSelector);
   const dispatch = useDispatch();
-
   const tokens = useSelector(tokensSelector);
   const allTokens = Object.values(tokens).reduce((all, current) => {
     return all.concat(current);
@@ -39,6 +51,8 @@ export const AssetsScreen = () => {
     };
   });
 
+  const [selectedCoin, setSelectedCoin] = useState<BaseCoin>();
+
   return (
     <BaseScreen>
       <View style={[t.flex1]}>
@@ -53,24 +67,39 @@ export const AssetsScreen = () => {
             />
           }>
           <Card>
-            {coins.map(coin => (
-              <TouchableOpacity
-                key={coin.id + coin.network}
-                style={[t.flexRow, t.itemsCenter, t.p1]}>
-                <Image
-                  source={{uri: coin.image}}
-                  style={[t.w8, t.h8, t.roundedFull, t.bgWhite, t.mR2]}
-                />
-                <View style={[t.flex1]}>
-                  <Paragraph text={coin.name} />
-                </View>
-                <Paragraph
-                  text={`${normalizeNumber(
-                    coin.balance || 0,
-                  )} ${coin.symbol.toUpperCase()}`}
-                />
-              </TouchableOpacity>
-            ))}
+            {coins.map(coin => {
+              const isSelected =
+                selectedCoin?.id === coin.id &&
+                selectedCoin.network === coin.network;
+              return (
+                <TouchableOpacity
+                  onPress={() => setSelectedCoin(coin)}
+                  key={coin.id + coin.network}
+                  style={[
+                    t.flexRow,
+                    t.itemsCenter,
+                    t.p1,
+                    t.pL2,
+                    t.pR2,
+                    t.roundedLg,
+                    isSelected ? {backgroundColor: colors.button} : {},
+                    isSelected ? shadow : {},
+                  ]}>
+                  <Image
+                    source={{uri: coin.image}}
+                    style={[t.w8, t.h8, t.roundedFull, t.bgWhite, t.mR2]}
+                  />
+                  <View style={[t.flex1]}>
+                    <Paragraph text={coin.name} />
+                  </View>
+                  <Paragraph
+                    text={`${normalizeNumber(
+                      coin.balance || 0,
+                    )} ${coin.symbol.toUpperCase()}`}
+                  />
+                </TouchableOpacity>
+              );
+            })}
             <TouchableOpacity
               style={[t.mT3, t.flexRow, t.justifyCenter, t.itemsCenter]}>
               <MIcon name="plus" size={20} color={colors.white} />
@@ -85,13 +114,13 @@ export const AssetsScreen = () => {
         </ScrollView>
       </View>
       <View>
-        <Button text="Tx Details" />
+        <Button text="Tx Details" disabled={!selectedCoin} />
         <View style={[t.flexRow, t.mT2]}>
           <View style={[t.flex1]}>
-            <Button text="Receive" />
+            <Button text="Receive" disabled={!selectedCoin} />
           </View>
           <View style={[t.flex1, t.mL2]}>
-            <Button text="Send" />
+            <Button text="Send" disabled={!selectedCoin} />
           </View>
         </View>
       </View>
