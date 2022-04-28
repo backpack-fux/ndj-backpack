0d02ff25ea3c9a1881d07af71d4f35741234f6ff
pei <peipeilong116@gmail.com>
2022-04-27 21:49:36 -0400

    fix: update received screen

--- src/screens/Assets/Receive.tsx
+++ src/screens/Assets/Receive.tsx
@@ -5,7 +5,7 @@ import {RouteProp, useNavigation, useRoute} from '@react-navigation/native';
 import React from 'react';
 import {useSelector} from 'react-redux';
 import {QRCode} from 'react-native-custom-qr-codes';
-import {TouchableOpacity, View} from 'react-native';
+import {ScrollView, TouchableOpacity, View} from 'react-native';
 import {t} from 'react-native-tailwindcss';
 import {colors} from '@app/assets/colors.config';
 import Icon from 'react-native-vector-icons/MaterialIcons';
@@ -32,51 +32,56 @@ export const ReceiveScreen = () => {
 
   return (
     <BaseScreen>
-      <View style={[t.flex1, t.itemsCenter, t.justifyCenter]}>
-        <View
-          style={[
-            t.bgPurple500,
-            t.itemsCenter,
-            t.justifyCenter,
-            t.p2,
-            t.roundedXl,
-          ]}>
-          <QRCode
-            content={wallet?.address}
-            outerEyeStyle="square"
-            innerEyeStyle="circle"
-            codeStyle="circle"
-            logo={logo}
-            logoSize={50}
-            backgroundColor={colors.primary}
-            linearGradient={['rgb(0,255,255)', colors.primaryLight]}
-            gradientDirection={[0, 300, 100, 0]}
-          />
-        </View>
-        <View
-          style={[
-            t.flexRow,
-            shadow,
-            t.bgPurple500,
-            t.p4,
-            t.roundedXl,
-            t.mT2,
-            t.border2,
-            t.borderPurple200,
-            t.itemsCenter,
-          ]}>
-          <Paragraph text={`${coin?.symbol.toUpperCase()} Address: `} />
-          <View style={[t.flex1]}>
-            <Paragraph
-              text={wallet?.address}
-              numberOfLines={1}
-              ellipsizeMode="middle"
+      <View style={[t.flex1]}>
+        <ScrollView
+          contentContainerStyle={[t.itemsCenter, t.justifyCenter]}
+          showsVerticalScrollIndicator={false}>
+          <View
+            style={[
+              t.bgPurple500,
+              t.itemsCenter,
+              t.justifyCenter,
+              t.p2,
+              t.roundedXl,
+            ]}>
+            <QRCode
+              content={wallet?.address}
+              outerEyeStyle="square"
+              innerEyeStyle="circle"
+              codeStyle="circle"
+              logo={logo}
+              logoSize={50}
+              backgroundColor={colors.primary}
+              linearGradient={['rgb(0,255,255)', colors.primaryLight]}
+              gradientDirection={[0, 300, 100, 0]}
             />
           </View>
-          <TouchableOpacity onPress={onCopy}>
-            <Icon name="content-copy" size={20} color={colors.white} />
-          </TouchableOpacity>
-        </View>
+          <View
+            style={[
+              t.flexRow,
+              shadow,
+              t.bgPurple500,
+              t.p4,
+              t.roundedXl,
+              t.mT2,
+              t.mB4,
+              t.border2,
+              t.borderPurple200,
+              t.itemsCenter,
+            ]}>
+            <Paragraph text={`${coin?.symbol.toUpperCase()} Address: `} />
+            <View style={[t.flex1]}>
+              <Paragraph
+                text={wallet?.address}
+                numberOfLines={1}
+                ellipsizeMode="middle"
+              />
+            </View>
+            <TouchableOpacity onPress={onCopy}>
+              <Icon name="content-copy" size={20} color={colors.white} />
+            </TouchableOpacity>
+          </View>
+        </ScrollView>
       </View>
       <View>
         <Button text="Back" onPress={() => navigation.goBack()} />
